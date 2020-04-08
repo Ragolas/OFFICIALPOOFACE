265da39f547e36a0f94c71b3713b915df800b67b
PAVLO <PAVLO>
2020-04-08 14:06:49 -0500

    COMMITMENT

---
+++ XCODE first/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XCODE first
+//
+//  Created by Pablo Ruiz-Velasco on 4/7/20.
+//  Copyright © 2020 JOE MAMA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("NIGGERS!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
